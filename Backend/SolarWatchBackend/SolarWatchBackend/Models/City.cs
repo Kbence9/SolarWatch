386797b0063bdb9b94543a516cbde7a94b665322
Kbence9 <bence.kristian@gmail.com>
2024-03-12 12:50:10 +0100

    feat - add City model

---
+++ Backend/SolarWatchBackend/SolarWatchBackend/Models/City.cs
@@ -0,0 +1,19 @@
+﻿namespace SolarWatchBackend.Models;
+
+public class City
+{
+    public string Name { get; init; }
+    public float Lat { get; init; }
+    public float Lng { get; init; }
+    public string Country { get; init; }
+    public string State { get; init; }
+
+    public City(string name, float lat, float lng, string country, string state)
+    {
+        Name = name;
+        Lat = lat;
+        Lng = lng;
+        Country = country;
+        State = state;
+    }
+}
