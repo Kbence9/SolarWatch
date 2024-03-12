94080dc8aa9d21ae6ecac470bd8b559fe15a7049
Kbence9 <bence.kristian@gmail.com>
2024-03-12 12:44:55 +0100

    feat - create JsonProcessor.cs from IJsonProcessor

---
+++ Backend/SolarWatchBackend/SolarWatchBackend/Services/JsonProcessor.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using SolarWatchBackend.Models;
+
+namespace WeatherApi.Services;
+
+public class JsonProcessor : IJsonProcessor
+{
+    
+    public SunSetRise ProcessSunSetRise(string data, string city, DateTime date)
+    {
+        JsonDocument json = JsonDocument.Parse(data);
+        JsonElement result = json.RootElement.GetProperty("results");
+
+        var sunSetRise = new SunSetRise(
+            city,
+            date,
+            result.GetProperty("sunrise").GetDateTime(),
+            result.GetProperty("sunset").GetDateTime()
+            );
+
+        return sunSetRise;
+    }
+    
+    private static DateTime GetDateTimeFromUnixTimeStamp(long timeStamp)
+    {
+        DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(timeStamp);
+        DateTime dateTime = dateTimeOffset.UtcDateTime;
+
+        return dateTime;
+    }
+}
