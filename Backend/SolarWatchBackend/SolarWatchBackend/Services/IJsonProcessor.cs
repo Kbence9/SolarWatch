dff2fb1bf0e26e01e0b5a1b4c99e1452a1d60384
Kbence9 <bence.kristian@gmail.com>
2024-03-12 12:41:15 +0100

    feat - add IJsonProcessor.cs

---
+++ Backend/SolarWatchBackend/SolarWatchBackend/Services/IJsonProcessor.cs
@@ -0,0 +1,8 @@
+﻿using SolarWatchBackend.Models;
+
+namespace WeatherApi.Services;
+
+public interface IJsonProcessor
+{
+    SunSetRise ProcessSunSetRise(string data, string city, DateTime date);
+}
