96efe02ade4facff4c7924476a39648d1b5df03c
Kbence9 <bence.kristian@gmail.com>
2024-03-12 13:14:23 +0100

    feat - add CityGeoCodeController.cs

---
+++ Backend/SolarWatchBackend/SolarWatchBackend/Controllers/CityGeoCodeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SolarWatchBackend.Models;
+using SolarWatchBackend.Services;
+
+namespace SolarWatchBackend.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class CityGeoCodeController : ControllerBase
+{
+    private readonly ILogger<CityGeoCodeController> _logger;
+    private readonly ICityProvider _cityProvider;
+
+    public CityGeoCodeController(ILogger<CityGeoCodeController> logger, ICityProvider cityProvider)
+    {
+        _logger = logger;
+        _cityProvider = cityProvider;
+    }
+
+    [HttpGet]
+    public ActionResult<CityGeoCode> GetCityGeoCode(string city)
+    {
+        try
+        {
+            var cityGeoCode = _cityProvider.GetCity(city);
+            return Ok(cityGeoCode.Result);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Error getting geocode data");
+            return Problem("Error getting geocode data");
+        }
+    }
+    
+}
