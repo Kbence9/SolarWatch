b60c4e25e6b8646388ca4ce4d55062a8c07267e5
Kbence9 <bence.kristian@gmail.com>
2024-03-25 12:20:31 +0100

    update - add services to the Program.cs

--- Backend/SolarWatchBackend/SolarWatchBackend/Program.cs
+++ Backend/SolarWatchBackend/SolarWatchBackend/Program.cs
@@ -1,11 +1,14 @@
 using System.Text;
 using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using SolarWatchBackend.Data;
 using SolarWatchBackend.Repositories;
 using SolarWatchBackend.Service;
 using SolarWatchBackend.Services;
+using SolarWatchBackend.Services.Authentication;
 
 var root = Directory.GetCurrentDirectory();
 var dotenv = Path.Combine(root, "secrets.env");
@@ -23,13 +26,41 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(option =>
+{
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "Bearer"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
 builder.Services.AddDbContext<UsersContext>();
 builder.Services.AddTransient<IJsonProcessor, JsonProcessor>();
 builder.Services.AddTransient<ISunSetRiseProvider, SunSetRiseProvider>();
 builder.Services.AddTransient<ICityProvider, CityProvider>();
 builder.Services.AddTransient<ICityRepository, CityRepository>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddTransient<ITokenService, TokenService>();
 
 builder.Services
     .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -49,6 +80,18 @@ builder.Services
             ),
         };
     });
+builder.Services
+    .AddIdentityCore<IdentityUser>(options =>
+    {
+        options.SignIn.RequireConfirmedAccount = false;
+        options.User.RequireUniqueEmail = true;
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 6;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireLowercase = false;
+    })
+    .AddEntityFrameworkStores<UsersContext>();
 
 
 var app = builder.Build();
