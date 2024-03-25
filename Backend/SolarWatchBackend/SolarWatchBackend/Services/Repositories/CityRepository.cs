03f44929ec32948dc5c0c112c0774a21616e8b69
Kbence9 <bence.kristian@gmail.com>
2024-03-25 11:38:22 +0100

    feat - add repository for the cities

---
+++ Backend/SolarWatchBackend/SolarWatchBackend/Services/Repositories/CityRepository.cs
@@ -0,0 +1,40 @@
+﻿using SolarWatchBackend.Data;
+using SolarWatchBackend.Models;
+
+namespace SolarWatchBackend.Repositories;
+
+public class CityRepository : ICityRepository
+{
+    public IEnumerable<City> GetAll()
+    {
+        using var dbContext = new SolarWatchContext();
+        return dbContext.Cities.ToList();
+    }
+
+    public City? GetByName(string name)
+    {
+        using var dbContext = new SolarWatchContext();
+        return dbContext.Cities.FirstOrDefault(c => c.Name == name);
+    }
+
+    public void Add(City city)
+    {
+        using var dbContext = new SolarWatchContext();
+        dbContext.Add(city);
+        dbContext.SaveChanges();
+    }
+
+    public void Delete(City city)
+    {
+        using var dbContext = new SolarWatchContext();
+        dbContext.Remove(city);
+        dbContext.SaveChanges();
+    }
+
+    public void Update(City city)
+    {  
+        using var dbContext = new SolarWatchContext();
+        dbContext.Update(city);
+        dbContext.SaveChanges();
+    }
+}
