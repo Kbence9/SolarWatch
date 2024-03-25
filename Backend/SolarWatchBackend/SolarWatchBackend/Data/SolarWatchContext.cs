605944c10d4a3c0c8997f8a4956cd13c2dc0499d
Kbence9 <bence.kristian@gmail.com>
2024-03-25 10:18:59 +0100

    feat - add SolarWatchContext.cs

---
+++ Backend/SolarWatchBackend/SolarWatchBackend/Data/SolarWatchContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SolarWatchBackend.Models;
+
+namespace SolarWatchBackend.Data;
+
+public class SolarWatchContext : DbContext
+{
+    public DbSet<City> Cities { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=localhost,1433;Database=SolarWatch;User Id=sa;Password=Benten10.;Encrypt=false;");
+    }
+    
+}
