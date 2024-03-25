6513d0a1e60fa76cd4e7a5abf2724e103c28344a
Kbence9 <bence.kristian@gmail.com>
2024-03-25 12:06:32 +0100

    feat - add UsersContext.cs

---
+++ Backend/SolarWatchBackend/SolarWatchBackend/Data/UsersContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SolarWatchBackend.Data;
+
+public class UsersContext : IdentityUserContext<IdentityUser>
+{
+    public UsersContext (DbContextOptions<UsersContext> options)
+        : base(options)
+    {
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseSqlServer("Server=localhost,1433;Database=WeatherApi;User Id=sa;Password=Benten10.;TrustServerCertificate=true;");
+
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
